aaa673a3b7902c588339665925c8ce925244d58b
Caio Chami <caio.chami18@gmail.com>
2022-12-22 09:58:18 -0300

    feat: Set up routes.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,10 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+
 export function Layout() {
-  return <div className="">Layout</div>;
+  return (
+    <div className="">
+      <Outlet />
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,34 @@ import ReactDOM from "react-dom/client";
 import { Layout } from "./components/Layout";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "/create",
+        element: <div>Create</div>,
+      },
+      {
+        path: "/:id",
+        element: <div>Show</div>,
+      },
+      {
+        path: "/:id/edit",
+        element: <div>Edit</div>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />,
   },
 ]);
 
